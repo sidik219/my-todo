5513391966ea0b8042a916cd3ee6f03c15517ed8
Sidik Mulyana <sidik.mlyn@gmail.com>
2022-09-12 18:20:40 +0700

    update env

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBGXsxaHgmf2vspdPqRHYzh7HgavY2bMqY",
-    authDomain: "my-todo-5d03a.firebaseapp.com",
-    projectId: "my-todo-5d03a",
-    storageBucket: "my-todo-5d03a.appspot.com",
-    messagingSenderId: "148172421534",
-    appId: "1:148172421534:web:ea5895f2d26bb739a5fe8f"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
